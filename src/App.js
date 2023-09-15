1f6cf80ba637e3c7f75eb194d9455b951d190dc6
Rebelkat18 <91990952+Rebelkat18@users.noreply.github.com>
2023-09-15 10:51:50 -0400

    Data and app restructure

--- src/App.js
+++ src/App.js
@@ -1,2 +1,20 @@
+import React from "react";
 import "./App.css";
-  
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1 class="Logo">Quiz City</h1>
+        <nav class="Navpage">
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Quizzes</a>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-import "./App";
-
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import App from "./App";
 import data from './data.json';
 console.log(data);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
+);
