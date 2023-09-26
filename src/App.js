4f6937a281e9b2977cdaeac231c79a86199af5bf
Rabayomi0 <riabayomi77@gmail.com>
2023-09-25 21:13:13 -0400

    Few Changes (9/25)
    Just a few changes here and there.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import React from "react";
 import "./App.css";
 
+
 function App() {
-  return (
+  
+  window.addEventListener("scroll", () => 
+  {document.body.style.setProperty('--scroll', window.scrollY/
+   (document.body.offsetHeight - window.innerHeight))
+  }
+  );
+   
+
+  return ( 
     <div>
       <header>
         <h1 class="Logo">Quiz City</h1>
         <nav class="Navpage">
-          <a href="#">Home</a>
-          <a href="#">About</a>
-          <a href="#">Quizzes</a>
+          <a href="/#">Home</a>
+          <a href="/#">About</a>
+          <a href="/#">Quizzes</a>
         </nav>
       </header>
-    </div>
-  );
-}
+      <section>
+        <img src="204.jpg" id="city" alt="Background of cities."></img>
+      </section>
+      </div>
+      );
+    }
 
 export default App;
 
