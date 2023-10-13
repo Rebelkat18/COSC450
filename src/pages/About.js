1f9145f66b1c3b95582a26f7e29ba11c06eeeba9
Rabayomi0 <riabayomi77@gmail.com>
2023-10-13 15:49:44 -0400

    Start of Window Details
    Window Shine

--- src/pages/About.js
+++ src/pages/About.js
@@ -17,14 +17,28 @@ function About() {
     <section className="All">
       <section className="wrapper">
         <div id="Window"></div>
-        <div id="leftwin" class="win"></div>
-        <div id="rightwin" class="win"></div>
+        <div id="leftwin" class="win">
+          <div class="shine"></div>
+          <div class="shine"></div>
+        </div>
+        <div id="rightwin" class="win">
+          <div class="shine"></div>
+          <div class="shine"></div>
+        </div>
+        
       </section>
 
       <section className="wrapper2">
           <div id="Window2"></div>
-          <div id="leftwin2" class="win"></div>
-          <div id= "rightwin2" class="win"></div>
+          <div id="leftwin2" class="win">
+            <div class="shine"></div>
+            <div class="shine"></div>
+            <div class="handle"></div>
+          </div>
+          <div id= "rightwin2" class="win">
+            <div class="shine"></div>
+            <div class="shine"></div>
+          </div>
         </section>
         </section>
       
