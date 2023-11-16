3bd88b835a3172370dc267412e68df4f763088a7
Rabayomi0 <riabayomi77@gmail.com>
2023-11-15 19:09:34 -0500

    Colors Minor Changes (Again)
    Less flashbang.

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -9,7 +9,6 @@ function Results() {
   var active = 0;
   var colors;
   
-  
   //sort total for highest score
   let sorted = total.sort((a, b) => (a.score < b.score ? 1 : -1));
   let result = sorted[0].result;
@@ -19,13 +18,23 @@ function Results() {
   if({...data.Quizzes[0]}){
   setInterval(() =>
   {document.querySelector('body').style.background = colors[active];
-  if(colors === "Black"){
-    document.querySelector('p').style.color = 'white';
+  if(colors == "Black"){
+    const el = document.querySelectorAll('a');
+    for(let i = 0; i < el.length; i++){
+      el[i].style.color = "red"
+    }
+    document.querySelector('h1').style.color = "red";
+  }
+  else if(colors == "Yellow" || colors == "White"){
+      const el = document.querySelectorAll('a');
+      for(let i = 0; i < el.length; i++){
+        el[i].style.color = "black"
+      }
+      document.querySelector('h1').style.color = "black";
   }
   active++;});
   }
   
-
   return (
     <div className="container">
       <header>
